02e3dd62037197dd0c1a4dece5f68e239ad4d963
Rafasloth <rafaslothcr@gmail.com>
2023-09-22 22:18:05 -0600

    setup for part 2

---
+++ Source/Chunks/Private/PatchingDemoGameInstance.cpp
@@ -0,0 +1,129 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PatchingDemoGameInstance.h"
+
+#include "ChunkDownloader.h"
+#include "Misc/CoreDelegates.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+
+void UPatchingDemoGameInstance::Init()
+{
+    Super::Init();
+    const FString DeploymentName = "PatchingDemoLive";
+    const FString ContentBuildId = "PatchingDemoKey";
+
+    // initialize the chunk downloader with chosen platform
+    TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetOrCreate();
+    Downloader->Initialize("Windows", 8);
+
+    // load the cached build ID
+    Downloader->LoadCachedBuild(DeploymentName);
+
+    // update the build manifest file
+    TFunction<void(bool bSuccess)> UpdateCompleteCallback = [&](bool bSuccess) {bIsDownloadManifestUpToDate = bSuccess; };
+    Downloader->UpdateBuild(DeploymentName, ContentBuildId, UpdateCompleteCallback);
+}
+
+void UPatchingDemoGameInstance::Shutdown()
+{
+    Super::Shutdown();
+    // Shut down ChunkDownloader
+    FChunkDownloader::Shutdown();
+}
+
+void UPatchingDemoGameInstance::OnManifestUpdateComplete(bool bSuccess)
+{
+    bIsDownloadManifestUpToDate = bSuccess;
+}
+
+void UPatchingDemoGameInstance::GetLoadingProgress(int32& BytesDownloaded, int32& TotalBytesToDownload, float& DownloadPercent, int32& ChunksMounted, int32& TotalChunksToMount, float& MountPercent) const
+{
+    //Get a reference to ChunkDownloader
+    TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetChecked();
+
+    //Get the loading stats struct
+    FChunkDownloader::FStats LoadingStats = Downloader->GetLoadingStats();
+
+    //Get the bytes downloaded and bytes to download
+    BytesDownloaded = LoadingStats.BytesDownloaded;
+    TotalBytesToDownload = LoadingStats.TotalBytesToDownload;
+
+    //Get the number of chunks mounted and chunks to download
+    ChunksMounted = LoadingStats.ChunksMounted;
+    TotalChunksToMount = LoadingStats.TotalChunksToMount;
+
+    //Calculate the download and mount percent using the above stats
+    DownloadPercent = ((float)BytesDownloaded / (float)TotalBytesToDownload) * 100.0f;
+    MountPercent = ((float)ChunksMounted / (float)TotalChunksToMount) * 100.0f;
+}
+
+bool UPatchingDemoGameInstance::PatchGame()
+{
+    // make sure the download manifest is up to date
+    if (bIsDownloadManifestUpToDate)
+    {
+        // get the chunk downloader
+        TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetChecked();
+
+        // report current chunk status
+        for (int32 ChunkID : ChunkDownloadList)
+        {
+            int32 ChunkStatus = static_cast<int32>(Downloader->GetChunkStatus(ChunkID));
+            UE_LOG(LogTemp, Display, TEXT("Chunk %i status: %i"), ChunkID, ChunkStatus);
+        }
+
+        TFunction<void(bool bSuccess)> DownloadCompleteCallback = [&](bool bSuccess) {OnDownloadComplete(bSuccess); };
+        Downloader->DownloadChunks(ChunkDownloadList, DownloadCompleteCallback, 1);
+
+        // start loading mode
+        TFunction<void(bool bSuccess)> LoadingModeCompleteCallback = [&](bool bSuccess) {OnLoadingModeComplete(bSuccess); };
+        Downloader->BeginLoadingMode(LoadingModeCompleteCallback);
+        return true;
+    }
+
+    // you couldn't contact the server to validate your Manifest, so you can't patch
+    UE_LOG(LogTemp, Display, TEXT("Manifest Update Failed. Can't patch the game"));
+
+    return false;
+}
+
+void UPatchingDemoGameInstance::OnLoadingModeComplete(bool bSuccess)
+{
+    OnDownloadComplete(bSuccess);
+}
+
+void UPatchingDemoGameInstance::OnMountComplete(bool bSuccess)
+{
+    OnPatchComplete.Broadcast(bSuccess);
+}
+
+void UPatchingDemoGameInstance::OnDownloadComplete(bool bSuccess)
+{
+    if (bSuccess)
+    {
+        UE_LOG(LogTemp, Display, TEXT("Download complete"));
+
+        // get the chunk downloader
+        TSharedRef<FChunkDownloader> Downloader = FChunkDownloader::GetChecked();
+        FJsonSerializableArrayInt DownloadedChunks;
+
+        for (int32 ChunkID : ChunkDownloadList)
+        {
+            DownloadedChunks.Add(ChunkID);
+        }
+
+        //Mount the chunks
+        TFunction<void(bool bSuccess)> MountCompleteCallback = [&](bool bSuccess) {OnMountComplete(bSuccess); };
+        Downloader->MountChunks(DownloadedChunks, MountCompleteCallback);
+
+        OnPatchComplete.Broadcast(true);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Display, TEXT("Load process failed"));
+
+        // call the delegate
+        OnPatchComplete.Broadcast(false);
+    }
+}
